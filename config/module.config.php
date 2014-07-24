33705e047589ccfe81e366577da78539cf9c8275
George Steel <george@net-glue.co.uk>
2014-07-24 21:58:35 +0100

    WIP

--- config/module.config.php
+++ config/module.config.php
@@ -2,4 +2,11 @@
 
 return array(
 
+    'prismic' => array(
+        //"api" => "https://example.prismic.io/api",
+        //"token" => 'Some Permanent Token',
+        //"clientId" => 'Some Client ID',
+        //"clientSecret" => 'Some Client Secret',
+    ),
+
 );
