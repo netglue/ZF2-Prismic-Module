582292f367fb619bd1392f8995c1084e55415f13
George Steel <george@net-glue.co.uk>
2014-08-12 17:25:50 +0100

    Bundle some useful methods into the Context class

--- src/NetgluePrismic/Context.php
+++ src/NetgluePrismic/Context.php
@@ -4,6 +4,9 @@ namespace NetgluePrismic;
 
 use Prismic\Api;
 use Prismic\Ref;
+use Prismic\Document;
+
+use NetgluePrismic\Exception;
 
 class Context implements ApiAwareInterface
 {
@@ -79,4 +82,94 @@ class Context implements ApiAwareInterface
     {
         return (string) $this->getRef();
     }
+
+    /**
+     * Return a single document with the given id at the current repo ref
+     * @param string $id
+     * @return Document|NULL
+     */
+    public function getDocumentById($id)
+    {
+        $query = sprintf('[[:d = at(document.id, "%s")]]', $id);
+        $api = $this->getPrismicApi();
+        $documents = $api->forms()->everything->query($query)->ref((string) $this->getRef())->submit();
+        if(count($documents->getResults())) {
+            // There should be only one!
+            return current($documents->getResults());
+        }
+
+        return NULL;
+    }
+
+    /**
+     * Return a single document for the given bookmark name
+     * @param string $bookmark
+     * @return Document
+     * @throws Exception\RuntimeException
+     */
+    public function getDocumentByBookmark($bookmark)
+    {
+        /**
+         * Either a string id or NULL
+         */
+        $documentId = $this->getPrismicApi()->bookmark($bookmark);
+        if(!$documentId) {
+            throw new Exception\RuntimeException(sprintf(
+                'The bookmark %s does not exist in this repository',
+                (string) $bookmark
+            ));
+        }
+        $document = $this->getDocumentById($documentId);
+
+        if(!$document) {
+            throw new Exception\RuntimeException(sprintf(
+                'No document could be found with the id %s referenced by the bookmark %s',
+                $documentId,
+                $bookmark
+            ));
+        }
+
+        return $document;
+    }
+
+    /**
+     * Whether the given document has been bookmarked
+     * @param Document|string $doc Either a Document instance or the ID of a document
+     * @return bool
+     */
+    public function isBookmarked($doc)
+    {
+        $name = $this->findBookmarkByDocument($doc);
+        return !empty($name);
+    }
+
+    /**
+     * Return the bookmark name for the given document
+     * @param Document|string $doc Either a Document instance or the ID of a document
+     * @return string|NULL
+     * @throws Exception\InvalidArgumentException
+     */
+    public function findBookmarkByDocument($doc)
+    {
+        if(!$doc instanceof Document) {
+            if(!$d = $this->getDocumentById($doc)) {
+                throw new Exception\InvalidArgumentException(sprintf(
+                    'Expected a document instance or a valid document id. Received %s',
+                    gettype($doc) . (is_scalar($doc) ? $doc : '')
+                ));
+            }
+            $doc = $d;
+        }
+        $id = $doc->getId();
+        foreach($this->getPrismicApi()->bookmarks() as $name => $target) {
+            if($id === $target) {
+                return $name;
+            }
+        }
+
+        return NULL;
+    }
+
+
+
 }
