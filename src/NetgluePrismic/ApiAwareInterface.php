1251823b8e44914de9584c9c495555ff917a079d
George Steel <george@net-glue.co.uk>
2014-08-12 11:59:46 +0100

    Api aware interface and trait

---
+++ src/NetgluePrismic/ApiAwareInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace NetgluePrismic;
+
+use Prismic\Api;
+
+interface ApiAwareInterface
+{
+
+    /**
+     * Set the Prismic Api Instance
+     * @param Api $api
+     * @return void
+     */
+    public function setPrismicApi(Api $api);
+
+    /**
+     * Return Prismic Api instance
+     * @return Api|NULL
+     */
+    public function getPrismicApi();
+
+}
