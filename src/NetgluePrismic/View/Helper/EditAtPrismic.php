940d975e4cf4650a176badd50aeee49e237ee14a
George Steel <george@net-glue.co.uk>
2014-08-13 13:42:23 +0100

    Docs and updates to the edit link helper

--- src/NetgluePrismic/View/Helper/EditAtPrismic.php
+++ src/NetgluePrismic/View/Helper/EditAtPrismic.php
@@ -18,8 +18,25 @@ class EditAtPrismic extends AbstractHelper
      */
     protected $document;
 
-    protected $template = '<a class="prismic-edit" title="Edit this document" href="%1$s">Edit %2$s</a>';
+    /**
+     * Template for the link in sprintf format
+     *
+     * First position is the link url, second position is the link text
+     * @var string
+     */
+    protected $template = '<a class="prismic-edit" title="Edit this document" href="%1$s">%2$s</a>';
 
+    /**
+     * Link text or markup to render with the anchor
+     * @var string
+     */
+    protected $linkText = 'Edit Document';
+
+    /**
+     * Set the link template markup
+     * @param string $template
+     * @return self
+     */
     public function setTemplate($template)
     {
         $this->template = (string) $template;
@@ -27,6 +44,23 @@ class EditAtPrismic extends AbstractHelper
         return $this;
     }
 
+    /**
+     * Set the link text
+     * @param string $text
+     * @return self
+     */
+    public function setLinkText($text)
+    {
+        $this->linkText = (string) $text;
+
+        return $this;
+    }
+
+    /**
+     * Set the document being linked to
+     * @param Document $document
+     * @return self
+     */
     public function setDocument(Document $document)
     {
         $this->document = $document;
@@ -34,11 +68,19 @@ class EditAtPrismic extends AbstractHelper
         return $this;
     }
 
+    /**
+     * Return linked document
+     * @return Document|NULL
+     */
     public function getDocument()
     {
         return $this->document;
     }
 
+    /**
+     * Render with the current document
+     * @return string
+     */
     public function render()
     {
         if($this->document) {
@@ -55,12 +97,18 @@ class EditAtPrismic extends AbstractHelper
                 $url['scheme'],
                 $url['host'],
                 $this->document->getId());
-            return sprintf($this->template, $url, 'Document');
+
+            return sprintf($this->template, $url, $this->linkText);
         }
 
         return '';
     }
 
+    /**
+     * Invoke - within views simply : echo $this->editAtPrismic($document);
+     * @param Document $document
+     * @return self
+     */
     public function __invoke(Document $document = NULL) {
         if($document) {
             $this->setDocument($document);
@@ -69,6 +117,10 @@ class EditAtPrismic extends AbstractHelper
         return $this;
     }
 
+    /**
+     * Render
+     * @return string
+     */
     public function __toString()
     {
         return $this->render();
