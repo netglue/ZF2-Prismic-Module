deb364286ee50c8a6f9a8d9fe8fa16f53b21d531
George Steel <george@net-glue.co.uk>
2014-10-08 16:16:02 +0100

    More tests for link resolver

---
+++ tests/NetgluePrismic/Mvc/LinkResolverTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace NetgluePrismic\Mvc;
+use NetgluePrismic\bootstrap;
+use Prismic\Fragment\Link\DocumentLink;
+
+class LinkResolverTest extends \Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase
+{
+    private $resolver;
+    private $context;
+
+    public function setUp()
+    {
+        $this->setApplicationConfig(include __DIR__ . '/../../TestConfig.php.dist');
+        parent::setUp();
+        $services = $this->getApplicationServiceLocator();
+        $this->resolver = $services->get('NetgluePrismic\Mvc\LinkResolver');
+        $this->context = $services->get('Prismic\Context');
+    }
+
+    public function testGetRouteNameFromBookmark()
+    {
+        $this->assertEquals('test-bookmark', $this->resolver->getRouteNameFromBookmark('unit-test-bookmark'));
+    }
+
+    /**
+     * @expectedException NetgluePrismic\Exception\RuntimeException
+     * @expectedExceptionMessage No route could be found for the bookmark
+     */
+    public function testGetRouteNameFromBookmarkThrowsExceptionForUnknownBookmark()
+    {
+        $this->resolver->getRouteNameFromBookmark('unknown-bookmark');
+    }
+
+    public function testGetRouteNameFromMask()
+    {
+        $this->assertEquals('test-mask', $this->resolver->getRouteNameFromMask('test'));
+    }
+
+    /**
+     * @expectedException NetgluePrismic\Exception\RuntimeException
+     * @expectedExceptionMessage No route could be found for the specified document mask
+     */
+    public function testGetRouteNameFromMaskThrowsExceptionForUnknownBookmark()
+    {
+        $this->resolver->getRouteNameFromMask('unknown-mask');
+    }
+
+    public function testResolveWithBookmark()
+    {
+        $expect = '/test-bookmark';
+        $doc = $this->context->getDocumentByBookmark('unit-test-bookmark');
+
+        $id = $doc->getId();
+        $type = $doc->getType();
+        $tags = $doc->getTags();
+        $slug = $doc->getSlug();
+        $isBroken = false;
+        $link = new DocumentLink($id, $type, $tags, $slug, $isBroken);
+
+        $url = $this->resolver->resolve($link);
+        $this->assertEquals($expect, $url);
+
+    }
+
+    public function testResolveWithMask()
+    {
+        $expect = '/masked/not/VDRjFSsAACoAfWqX';
+        $doc = $this->context->getDocumentById('VDRjFSsAACoAfWqX');
+        // Mask == test
+
+        $id = $doc->getId();
+        $type = $doc->getType();
+        $tags = $doc->getTags();
+        $slug = $doc->getSlug();
+        $isBroken = false;
+        $link = new DocumentLink($id, $type, $tags, $slug, $isBroken);
+
+        $url = $this->resolver->resolve($link);
+        $this->assertEquals($expect, $url);
+
+    }
+}
--- tests/config/module.config.global.php
+++ tests/config/module.config.global.php
@@ -18,4 +18,31 @@ return array(
             'layout/layout' => __DIR__ . '/../data/dummy-error-template.phtml',
         ),
     ),
+
+    'router' => array(
+        'routes' => array(
+            'test-bookmark' => array(
+                'type' => 'Literal',
+                'options' => array(
+                    'route' => '/test-bookmark',
+                    'defaults' => array(
+                        'controller' => 'NetgluePrismic\TestController',
+                        'action' => 'bookmarkAction',
+                        'bookmark' => 'unit-test-bookmark',
+                    ),
+                ),
+            ),
+            'test-mask' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/masked/:slug/:prismic-id',
+                    'defaults' => array(
+                        'controller' => 'NetgluePrismic\TestController',
+                        'action' => 'maskAction',
+                        'mask' => 'test',
+                    ),
+                ),
+            ),
+        ),
+    ),
 );
