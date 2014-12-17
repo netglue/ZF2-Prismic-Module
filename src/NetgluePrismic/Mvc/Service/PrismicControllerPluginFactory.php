db5d04602a06f56b682c6e94ee13f7480a30d1ab
George Steel <george@net-glue.co.uk>
2014-12-17 14:34:13 +0000

    Fix BC breaks in constructor signature for DocumentLink in Prismic SDK

--- src/NetgluePrismic/Mvc/Service/PrismicControllerPluginFactory.php
+++ src/NetgluePrismic/Mvc/Service/PrismicControllerPluginFactory.php
@@ -17,15 +17,18 @@ class PrismicControllerPluginFactory implements FactoryInterface
     public function createService(ServiceLocatorInterface $controllerPluginManager)
     {
         $serviceLocator = $controllerPluginManager->getServiceLocator();
-        $context = $serviceLocator->get('Prismic\Context');
+        $context = $serviceLocator->get('NetgluePrismic\Context');
         $routingOptions = $serviceLocator->get('NetgluePrismic\Mvc\Router\RouterOptions');
         $linkResolver = $serviceLocator->get('NetgluePrismic\Mvc\LinkResolver');
+        $linkGenerator = $serviceLocator->get('NetgluePrismic\Mvc\LinkGenerator');
 
         $plugin = new Prismic;
         $plugin->setContext($context);
         $plugin->setPrismicApi($context->getPrismicApi());
         $plugin->setRouterOptions($routingOptions);
         $plugin->setLinkResolver($linkResolver);
+        $plugin->setLinkGenerator($linkGenerator);
+
 
         return $plugin;
     }
